265a7a4c36420e022b8615025e4063971cb96592
Chanderpaul Campbell <chandap2011@gmail.com>
2019-11-29 08:50:12 -0500

    added css , html and basic javascript and ajax

--- index.php
+++ index.php
@@ -1,1 +1,1 @@
-<h1>This is the beginning of something big</h1>
+word<!-- <h1>This is the beginning of something big</h1> -->
---
+++ login.php
@@ -0,0 +1,43 @@
+<?php
+$host = "localhost";
+$username = 'root';
+$password = '';
+$dbname = 'bugme';
+
+
+function test_input($data){
+  $data = trim($data);
+  $data = strip_tags($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+
+$userUN = test_input($_POST["username"]);
+$userPW = test_input($_POST["password"]);
+
+
+// if($cities == "cities"){
+//   echo "mhm";
+// }
+
+// $userUN = "admin";
+// $userPW = "password123";
+
+// echo $country;
+$conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
+
+$stmt = $conn->query("SELECT * FROM users WHERE firstname LIKE '%$userUN%'");
+
+$results = $stmt -> fetchall(PDO::FETCH_ASSOC);
+
+// var_dump($results);
+
+foreach($results as $row):
+  if ($row["password"] == $userPW){
+    $response = "valid";
+    echo $response;
+    // echo "valid";
+
+  }
+endforeach; ?>
